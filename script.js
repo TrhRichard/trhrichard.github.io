80f482c35a715af040ce09c2c40418d80769b035
Richard <gamingradu1@gmail.com>
2023-02-24 18:55:53 +0200

    Updated Website

---
+++ script.js
@@ -0,0 +1,7 @@
+const minecraft2dplay = document.getElementById("minecraft2dplay")
+const fuwafuwa = document.getElementById("fuwafuwa")
+
+minecraft2dplay.addEventListener("click", function(){
+    console.log("hello")
+    window.location.href = "minecraft2d/index.html"
+})
