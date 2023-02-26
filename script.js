13b022f87a7bd1567180e89a4187f7a9a029234a
Richard <gamingradu1@gmail.com>
2023-02-26 15:37:52 +0200

    fixed redirect

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const minecraft2dplay = document.getElementById("minecraft2dplay")
 const fuwafuwa = document.getElementById("fuwafuwa")
 
+fuwafuwa.addEventListener("click", function(){
+    //window.location.href = "anime/fuwafuwa.mp4"
+    window.location.replace("anime/fuwafuwa.mp4");
+})
+
 minecraft2dplay.addEventListener("click", function(){
-    console.log("hello")
-    window.location.href = "minecraft2d/index.html"
+    //window.location.href = "minecraft2d"
+    window.location.replace("minecraft2d");
 })
