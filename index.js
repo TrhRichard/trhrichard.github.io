af2cfa629d8cae6117dfd01744c917e006eb3fb3
Richard <62023434+TrhRichard@users.noreply.github.com>
2024-08-19 18:12:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const random_texts = [
     "ping @trhrichard if u want something idk",
     "hi there",
-    ":SubaruTeehee:"
+    ":SubaruTeehee:",
+    "jambo was here",
+    "jambie guessed the word!",
+    "harewataru guessed the word!",
+    "so you know when you go to dinner?"
 ]
 
 document.querySelector("#randomtext").innerHTML = random_texts[Math.floor(Math.random() * random_texts.length)]
