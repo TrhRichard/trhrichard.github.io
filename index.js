90f530772f62233508a9fb1a986054514c5b8780
Richard <62023434+TrhRichard@users.noreply.github.com>
2024-08-21 23:41:29 +0300

    Added more random texts & a box around the subarus

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const random_texts = [
     "jambie guessed the word!",
     "harewataru guessed the word!",
     "so you know when you go to dinner?",
-    "you can refresh the page for more messages"
+    "you can refresh the page for more messages",
+    "give frazzle money",
+    "estimated time: ∞"
 ]
 
 document.querySelector("#randomtext").innerHTML = random_texts[Math.floor(Math.random() * random_texts.length)]
